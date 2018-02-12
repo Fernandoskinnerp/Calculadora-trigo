e86e8aebb444c7481467bea417328c1805fae39e
Estudiantes <Estudiantes@Labing70202>
2018-02-12 18:08:09 -0500

    inicio calculadora

---
+++ src/micalculadora/Calculadora.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package micalculadora;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Calculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    protected float x;
+    private float y;
+    private float r;
+
+    void sumar() {
+        r = x + y;
+    }
+
+    void restar() {
+         r = x - y;
+       
+        }       
+}
